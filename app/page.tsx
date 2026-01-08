0df9403752dbf7f8c7d06d8851f457a3fdd5a39d
Sudhan Pariyar <sudhanratan123@gmail.com>
2026-01-08 23:11:13 +1100

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+return <div className="flex items-center justify-center min-h-screen">
+  <h1 className="text-5xl font-semibold text-shadow-xs text-neutral-800">
+    QControl
+  </h1>
+</div>
+}
